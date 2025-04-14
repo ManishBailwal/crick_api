993c2ba2824eacc78a1226b46615774b83339a54
ManishBailwal <102870593+ManishBailwal@users.noreply.github.com>
2025-04-14 12:01:46 +0530

    pushing final code

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import joblib
 import pandas as pd
+import os
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
@@ -80,4 +81,5 @@ def predict():
     })
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
